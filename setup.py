fe5bf2ff7c64652369a102df8bb247c4ae92f031
Michael Pratt <michael@pratt.im>
2014-12-23 10:41:57 -0500

    Add barebones ghostblog module

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ghostblog',
+      version='0.1.0',
+      description='Python Ghost API wrapper',
+      author='Michael Pratt',
+      author_email='michael@pratt.im',
+      packages=['ghostblog'],
+)
